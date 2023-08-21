40885b990912858115d3d02c9c01df199694fe5d
mokasp <kasperbouldin@gmail.com>
2023-08-21 00:44:52 -0700

    added find all inner nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	else
+		return ((binary_tree_nodes(tree->left)) + (binary_tree_nodes(tree->right)) + 1);
+}
