a8bb303699b5c0853320c7132a47cb9513f66c9e
mokasp <kasperbouldin@gmail.com>
2023-08-21 00:12:04 -0700

    added find num of leaves

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	leaves = (binary_tree_leaves(tree->left)) + (binary_tree_leaves(tree->right));
+	return (leaves);
+}
+
